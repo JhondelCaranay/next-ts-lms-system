2185ae59b02191180b3c79875dc2060028d943a7
jhondel <jhondeldelconacaranay@gmail.com>
2023-10-03 03:16:09 +0800

    course edit page ui

--- src/app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ src/app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -1,12 +1,17 @@
 import { prisma } from "@/lib/prisma";
 import { auth } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
+import Actions from "./_components/actions";
+import { CircleDollarSign, File, LayoutDashboard, ListChecks } from "lucide-react";
+import { IconBadge } from "@/components/icon-badge";
 
 type CourseIdPageProps = {
-  courseId: string;
+  params: {
+    courseId: string;
+  };
 };
 
-const CourseIdPage = async ({ courseId }: CourseIdPageProps) => {
+const CourseIdPage = async ({ params }: CourseIdPageProps) => {
   const { userId } = auth();
 
   if (!userId) {
@@ -15,7 +20,7 @@ const CourseIdPage = async ({ courseId }: CourseIdPageProps) => {
 
   const course = await prisma.course.findUnique({
     where: {
-      id: courseId,
+      id: params.courseId,
       userId,
     },
     include: {
@@ -42,6 +47,81 @@ const CourseIdPage = async ({ courseId }: CourseIdPageProps) => {
     return redirect("/");
   }
 
-  return <div>CourseIdPage</div>;
+  const requiredFields = [
+    course.title,
+    course.description,
+    course.imageUrl,
+    course.price,
+    course.categoryId,
+    // course.chapters.some(chapter => chapter.isPublished),
+  ];
+
+  const totalFields = requiredFields.length;
+  const completedFields = requiredFields.filter(Boolean).length;
+
+  const completionText = `(${completedFields}/${totalFields})`;
+
+  const isComplete = requiredFields.every(Boolean);
+
+  return (
+    <>
+      <div className="p-6">
+        <div className="flex items-center justify-between">
+          <div className="flex flex-col gap-y-2">
+            <h1 className="text-2xl font-medium">Course setup</h1>
+            <span className="text-sm text-slate-700">Complete all fields {completionText}</span>
+          </div>
+          <Actions
+          // disabled={!isComplete}
+          // courseId={params.courseId}
+          // isPublished={course.isPublished}
+          />
+        </div>
+
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-16">
+          <div>
+            <div className="flex items-center gap-x-2">
+              <IconBadge icon={LayoutDashboard} />
+              <h2 className="text-xl">Customize your course</h2>
+            </div>
+            {/* <TitleForm initialData={course} courseId={course.id} /> */}
+            {/* <DescriptionForm initialData={course} courseId={course.id} /> */}
+            {/* <ImageForm initialData={course} courseId={course.id} /> */}
+            {/* <CategoryForm
+              initialData={course}
+              courseId={course.id}
+              options={categories.map((category) => ({
+                label: category.name,
+                value: category.id,
+              }))}
+            /> */}
+          </div>
+          <div className="space-y-6">
+            <div>
+              <div className="flex items-center gap-x-2">
+                <IconBadge icon={ListChecks} />
+                <h2 className="text-xl">Course chapters</h2>
+              </div>
+              {/* <ChaptersForm initialData={course} courseId={course.id} /> */}
+            </div>
+            <div>
+              <div className="flex items-center gap-x-2">
+                <IconBadge icon={CircleDollarSign} />
+                <h2 className="text-xl">Sell your course</h2>
+              </div>
+              {/* <PriceForm initialData={course} courseId={course.id} /> */}
+            </div>
+            <div>
+              <div className="flex items-center gap-x-2">
+                <IconBadge icon={File} />
+                <h2 className="text-xl">Resources & Attachments</h2>
+              </div>
+              {/* <AttachmentForm initialData={course} courseId={course.id} /> */}
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 export default CourseIdPage;
