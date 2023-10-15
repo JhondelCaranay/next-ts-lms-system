faed857c6765eeb878a1ceff93fcd448db98d414
jhondel <jhondeldelconacaranay@gmail.com>
2023-10-15 22:57:25 +0800

    categories query

--- src/app/(dashboard)/(routes)/search/page.tsx
+++ src/app/(dashboard)/(routes)/search/page.tsx
@@ -1,4 +1,44 @@
-const SearchPage = () => {
-  return <div>SearchPage</div>;
+import { SearchInput } from "@/components/search-input";
+import { prisma } from "@/lib/prisma";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import { Categories } from "./_components/categories";
+import { getCourses } from "@/actions/get-courses";
+
+type SearchPageProps = {
+  searchParams: {
+    title: string;
+    categoryId: string;
+  };
+};
+const SearchPage = async ({ searchParams }: SearchPageProps) => {
+  const { userId } = auth();
+
+  if (!userId) {
+    return redirect("/");
+  }
+
+  const categories = await prisma.category.findMany({
+    orderBy: {
+      name: "asc",
+    },
+  });
+
+  // const courses = await getCourses({
+  //   userId,
+  //   ...searchParams,
+  // });
+
+  return (
+    <>
+      <div className="px-6 pt-6 md:hidden md:mb-0 block">
+        <SearchInput />
+      </div>
+      <div className="p-6 space-y-4">
+        <Categories items={categories} />
+        {/* <CoursesList items={courses} /> */}
+      </div>
+    </>
+  );
 };
 export default SearchPage;
